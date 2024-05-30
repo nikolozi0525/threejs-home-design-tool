d05c06a20b43a1e2785207e89d4ed05c5694c0cc
nikolozi <nikolozi0622@gmail.com>
2024-05-29 17:57:59 -0700

    RoomDetails, Selection, wall options

---
+++ src/components/Basic/Wall/index.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+const Wall = ({}) => {
+  return (
+    <>
+      <div class="relative flex flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+        <div class="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+        <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 w-5/12">
+          <div class="mx-auto max-w-md">
+            <div>
+              <p class="font-bold">Wall Options:</p>
+              <p class="text-gray-400 w-1/2">
+                Please select the type of of wall you would like
+              </p>
+              <div class="space-y-6 py-8 text-base leading-7 text-gray-600">
+                <ul class="flex flex-wrap">
+                  <li class="flex items-center bg-gray-50 rounded-xl p-2 w-1/2">
+                    <div class="w-full bg-white rounded-xl">
+                      <div>
+                        <p class="text-3xl m-7">No Wall</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="flex items-center bg-gray-50 rounded-xl p-2 w-1/2">
+                    <div class="w-full bg-white rounded-xl">
+                      <div>
+                        <p class="text-3xl m-7">No Wall</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="flex items-center bg-gray-50 rounded-xl p-2 w-1/2">
+                    <div class="w-full bg-white rounded-xl">
+                      <div>
+                        <p class="text-3xl m-7">No Wall</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="flex items-center bg-gray-50 rounded-xl p-2 w-1/2">
+                    <div class="w-full bg-white rounded-xl">
+                      <div>
+                        <p class="text-3xl m-7">No Wall</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="flex items-center bg-gray-50 rounded-xl p-2 w-1/2">
+                    <div class="w-full bg-white rounded-xl">
+                      <div>
+                        <p class="text-3xl m-7">No Wall</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="flex items-center bg-gray-50 rounded-xl p-2 w-1/2">
+                    <div class="w-full bg-white rounded-xl">
+                      <div>
+                        <p class="text-3xl m-7">No Wall</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="flex items-center bg-gray-50 rounded-xl p-2 w-1/2">
+                    <div class="w-full bg-white rounded-xl">
+                      <div>
+                        <p class="text-3xl m-7">No Wall</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="flex items-center bg-gray-50 rounded-xl p-2 w-1/2">
+                    <div class="w-full bg-white rounded-xl">
+                      <div>
+                        <p class="text-3xl m-7">No Wall</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="flex items-center bg-gray-50 rounded-xl p-2 w-1/2">
+                    <div class="w-full bg-white rounded-xl">
+                      <div>
+                        <p class="text-3xl m-7">No Wall</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="flex items-center bg-gray-50 rounded-xl p-2 w-1/2">
+                    <div class="w-full bg-white rounded-xl">
+                      <div>
+                        <p class="text-3xl m-7">No Wall</p>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Wall;
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,13 +2,14 @@
 // import { fadeIn } from "../utils/motion";
 // import { TypeAnimation } from "react-type-animation";
 import RoomDetails from "./Basic/RoomDetails";
+import RoomSelection from "./Basic/RoomSelection";
 
 const Body = () => {
   return (
-    <div className="grid grid-cols-12">
-      <div className="sm:col-span-9 bg-gray-200"></div>
-      <div className="sm:col-span-3 w-full">
-        <RoomDetails />
+    <div className="flex flex-wrap">
+      <div className="w-full sm:w-8/12"></div>
+      <div className="w-full sm:w-4/12">
+        <RoomSelection />
       </div>
     </div>
   );
