2d9425ee50fdd6bd29c4ce145033d533b4ed92ef
nikolozi <nikolozi0525@gmail.com>
2024-05-30 06:26:01 +0400

    create hexagon div stretch

--- src/components/Basic/RoomHexagonGrid/index.jsx
+++ src/components/Basic/RoomHexagonGrid/index.jsx
@@ -1,22 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import "./styles.css";
 
-class RoomHexagonGrid extends Component {
-  render() {
-    return (
-      <div className="room-grid">
-        <div className="container">
-          {Array(100)
-            .fill(0)
-            .map(() => (
-              <div>
-                <span className="inner"></span>
-              </div>
-            ))}
-        </div>
+const RoomHexagonGrid = () => {
+  return (
+    <div className="room-grid">
+      <div className="container">
+        {Array(400)
+          .fill(0)
+          .map((_, index) => (
+            <div className="hexagon" key={index}>
+              {/* <>
+                  <span className={`inner`}></span>
+                  <div className="line"></div>
+                </> */}
+              <span className={`inner`}></span>
+            </div>
+          ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default RoomHexagonGrid;
