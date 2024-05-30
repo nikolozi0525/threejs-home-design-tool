dd9a8f6015015a71036835562ea6b791ed8d7e65
nikolozi <nikolozi0525@gmail.com>
2024-05-30 04:43:45 +0400

    Recreate floor plan grid

--- src/components/Basic/RoomHexagonGrid/index.jsx
+++ src/components/Basic/RoomHexagonGrid/index.jsx
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
-import { HexGrid, Layout, Hexagon, GridGenerator } from "react-hexgrid";
-import { TransitionGroup as CSSTransitionGroup } from "react-transition-group";
 import "./styles.css";
 
 class RoomHexagonGrid extends Component {
   render() {
-    const hexagons = GridGenerator.rectangle(100, 100);
     return (
-      <div className="roomgrid grow">
-        <HexGrid width="100%" height="100%" viewBox="">
-          <Layout
-            size={{ x: 60, y: 60 }}
-            // origin={{ x: -15, y: -40 }}
-            spacing={1.08}
-            flat={false}
-          >
-            {hexagons.map((hex, i) => (
-              <Hexagon key={i} {...hex} />
+      <div className="room-grid">
+        <div className="container">
+          {Array(100)
+            .fill(0)
+            .map(() => (
+              <div>
+                <span className="inner"></span>
+              </div>
             ))}
-          </Layout>
-        </HexGrid>
+        </div>
       </div>
     );
   }
