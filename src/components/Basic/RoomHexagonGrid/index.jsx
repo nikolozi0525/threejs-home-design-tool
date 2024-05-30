826be9f3ee8a53ee422e152346fb5adc6e54265b
nikolozi <nikolozi0525@gmail.com>
2024-05-30 17:23:47 +0400

    Drag & Drop grid each other

--- src/components/Basic/RoomHexagonGrid/index.jsx
+++ src/components/Basic/RoomHexagonGrid/index.jsx
@@ -13,64 +13,84 @@ const RoomHexagonGrid = () => {
     { index: 34, room: "Double Bathroom", color: "#EAB8B8" },
   ]); // {index: hexagon_id, room: room_name, color: room_color }
 
+  const onDragOver = (ev) => {
+    ev.preventDefault();
+    if (!ev.target.className.includes("drag-hover")) {
+      ev.target.className += "drag-hover";
+    }
+  };
+
+  const onDragLeave = (ev) => {
+    ev.preventDefault();
+
+    ev.target.className = ev.target.className.replace(/drag-hover/g, "");
+  };
+
+  const onDragStart = (ev, roomInfo) => {
+    ev.dataTransfer.setData("selectedRoom", roomInfo.room);
+    ev.dataTransfer.setData("selectedRoomColor", roomInfo.color);
+
+    setRoomData(roomData.filter((one) => one.index != roomInfo.index));
+    // ev.target.style.backgroundColor = `#fafafa71`;
+  };
+
+  const onDrop = (ev, index) => {
+    ev.preventDefault();
+    const selectedRoom = ev.dataTransfer.getData("selectedRoom");
+    const selectedRoomColor = ev.dataTransfer.getData("selectedRoomColor");
+
+    console.log("####", selectedRoom, selectedRoomColor);
+    if (selectedRoom) {
+      setRoomData([
+        ...roomData,
+        { index, room: selectedRoom, color: selectedRoomColor },
+      ]);
+
+      ev.target.style.backgroundColor = `${selectedRoomColor}`;
+      ev.target.className = ev.target.className.replace(/drag-hover/g, "");
+    } else {
+      ev.target.className = ev.target.className.replace(/drag-hover/g, "");
+    }
+  };
+
   return (
     <div className="room-grid">
       <div className="container">
         {Array(400)
           .fill(0)
-          .map((_, index) => (
-            <div
-              className={`hexagon`}
-              key={index}
-              onDragOver={(ev) => {
-                ev.preventDefault();
-                if (!ev.target.className.includes("drag-hover")) {
-                  ev.target.className += "drag-hover";
-                }
-              }}
-              onDragLeave={(ev) => {
-                ev.preventDefault();
-                ev.target.className = ev.target.className.replace(
-                  /drag-hover/g,
-                  ""
-                );
-              }}
-              onDrop={(ev) => {
-                ev.preventDefault();
-                const selectedRoom = ev.dataTransfer.getData("selectedRoom");
-                const selectedRoomColor =
-                  ev.dataTransfer.getData("selectedRoomColor");
+          .map((_, index) => {
+            const existRoomData = roomData.find((one) => {
+              return one.index == index;
+            });
+            return (
+              <div
+                className={`hexagon`}
+                key={index}
+                onDragOver={onDragOver}
+                onDragLeave={onDragLeave}
+                onDragStart={(ev) => onDragStart(ev, existRoomData)}
+                onDrop={(ev) => onDrop(ev, index)}
+                draggable
+              >
+                {/* <>
+                    <span className={`inner`}></span>
+                    <div className="line"></div>
+                  </> */}
 
-                setRoomData([
-                  ...roomData,
-                  { index, room: selectedRoom, color: selectedRoomColor },
-                ]);
-
-                ev.target.style.backgroundColor = `${selectedRoomColor}`;
-              }}
-            >
-              {/* <>
-                  <span className={`inner`}></span>
-                  <div className="line"></div>
-                </> */}
-
-              <span
-                id={`grid-inner-${index}`}
-                className={`inner `}
-                style={
-                  roomData.find((one) => {
-                    return one.index == index;
-                  })
-                    ? {
-                        backgroundColor: roomData.find(
-                          (one) => one.index == index
-                        ).color,
-                      }
-                    : {}
-                }
-              ></span>
-            </div>
-          ))}
+                <span
+                  id={`grid-inner-${index}`}
+                  className={`inner `}
+                  style={
+                    existRoomData
+                      ? {
+                          backgroundColor: existRoomData.color,
+                        }
+                      : {}
+                  }
+                ></span>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
